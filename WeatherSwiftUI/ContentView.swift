9abe989ca8c0390d668d72771ace467321b7c9f7
vengateshkr <vsubbarayan@humana.com>
2020-02-20 12:08:24 -0300

    Added initial setup

---
+++ WeatherSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherSwiftUI
+//
+//  Created by Gabriel on 2/20/20.
+//  Copyright © 2020 TCS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
